eb602426b308f380c136c0d0af963d87b411800a
matt2112 <matt_lewis@live.co.uk>
2016-10-03 15:39:35 +0100

    replace animate.css with jquery code

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
-// Close menu when item clicked.
-$('.nav-item').on('click', function (e) {
-    $('#collapsingNavbar').collapse('hide');
-});
+$(document).ready(function() {
+    
+    // Fade in summary text
+    $('#summary').hide(0).delay(200).fadeIn(1500);
+    
+    // Slide featured projects in from bottom
+    $('#fp-inner-container').animate({marginTop:"-=500px"}, 1200);
+                  
+    // Close menu when item clicked.
+    $('.nav-item').on('click', function (e) {
+        $('#collapsingNavbar').collapse('hide');
+    });
 
-// Smooth page scrolling using jQuery Easing plugin
-$('.nav-link').on('click', function (event) {
-    var $anchor = $(this).attr('href');
-    $('body').animate({
-        // offset height of navbar
-        scrollTop: $($anchor).offset().top - 53
-    }, 1200, 'easeInOutExpo');
-    event.preventDefault();
+    // Smooth page scrolling using jQuery Easing plugin
+    $('.nav-link').on('click', function (event) {
+        var $anchor = $(this).attr('href');
+        $('body').animate({
+            // offset height of navbar
+            scrollTop: $($anchor).offset().top - 53
+        }, 1200, 'easeInOutExpo');
+        event.preventDefault();
+    });
 });
