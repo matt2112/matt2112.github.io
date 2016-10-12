8caca2fa9b3a98d60f87239f0dabc8fa043a33b0
matt2112 <matt_lewis@live.co.uk>
2016-10-12 10:57:52 +0100

    restructure image folder add new background images remove profile picture add transparent bar in home page

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
     
+    // Cycle through fixed background images on body
+    $.backstretch([
+        "img/bg/beach.jpeg",
+        "img/bg/britmuseum.jpeg",
+        "img/bg/city1.jpeg",
+        "img/bg/lake.jpeg",
+        "img/bg/london1.jpeg",
+        "img/bg/mountains1.jpeg",
+        "img/bg/mountains2.jpeg",
+        "img/bg/natmuseum.jpeg"
+        ], {
+        fade: 1500,
+        duration: 4000
+    });
+
     // Fade in summary text
     $('#summary').delay(200).fadeIn(1500);
-    
+
     // Slide featured projects in from bottom
-    $('#fp-inner-container').animate({marginTop:"-=500px"}, 1200);
-                  
+    $('#fp-inner-container').animate({
+        marginTop: "-=500px"
+    }, 1200);
+
     // Close menu when item clicked.
     $('.nav-item').on('click', function (e) {
         $('#collapsingNavbar').collapse('hide');
@@ -20,10 +37,10 @@ $(document).ready(function() {
         }, 1200, 'easeInOutExpo');
         event.preventDefault();
     });
-    
+
     // Fade in other projects when opened
     var opclicked = false;
-    $('#op-header').on('click', function() {
+    $('#op-header').on('click', function () {
         if (!opclicked) {
             $('#collapse').fadeIn(1000);
             opclicked = true;
